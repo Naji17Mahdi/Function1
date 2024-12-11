920ab7d2ee92384f092dd8531fb77c44a5a2c423
Mahdi Naji <mahdinaji754@gmail.com>
2024-12-11 23:23:23 +0330

    Create Function1.cpp

---
+++ Function1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+int f ( int x )
+{
+	return 2 * x ;
+}
+
+main ()
+{
+	int a = 2 ;
+	int b ;
+	
+	
+	b = f ( a );
+	
+	
+	cout<<b;
+	
+}
